9629ef8b910fdb56d6335c787a371ff90bb05e62
Carmen Valente <carmenvalente@rentalcars.com>
2017-04-06 14:12:49 +0100

    does something

--- php/heartRateCalculator.php
+++ php/heartRateCalculator.php
@@ -2,32 +2,50 @@
 
 class heartRateCalculator
 {
-
+    /**
+     * @param $heartRate
+     * @param $age
+     * @return array
+     */
     public function getRates($heartRate, $age){
         $percentages = array(
-            55,
-            60,
-            65,
-            70,
-            75,
-            80,
-            85,
-            90,
-            95
+            0.55,
+            0.60,
+            0.65,
+            0.70,
+            0.75,
+            0.80,
+            0.85,
+            0.90,
+            0.95
         );
         $output = array();
 
+        $count=0;
+
         foreach($percentages as $intensity){
-            $values = $this->calculateRates($intensity, $heartRate, $age);
-            $output['intensity'] = $intensity;
-            $output['idealRate'] = $values;
+//            $values = $this->calculateRates($intensity, $heartRate, $age);
+            $output[$count]['intensity'] = $intensity;
+            $output[$count]['idealRate'] = $this->calculateRates($intensity, $heartRate, $age);
+            $count++;
         }
 
         return $output;
     }
 
+    /**
+     * @param $intensity
+     * @param $heartRate
+     * @param $age
+     * @return int
+     */
     public function calculateRates($intensity, $heartRate, $age){
-        $target = (((220 - $age) - $heartRate) * $intensity) + $heartRate;
+        if ($age+$heartRate == 220){
+            $target = 'error';
+        } else {
+            $target = 220;
+            $target = (($target - $age - $heartRate) * $intensity) + $heartRate;
+        }
 
         return $target;
     }
--- php/page.php
+++ php/page.php
@@ -1,11 +1,29 @@
 <?php
-use heartRateCalculator;
+require_once('heartRateCalculator.php');
+
+$output = "<!DOCTYPE html>" . PHP_EOL
+    . "<html lang=en> " . PHP_EOL
+    . "<body>" . PHP_EOL;
+
+$age = $_POST['age'];
+$RHR = $_POST['restHRate'];
+
+$output .= "<h1>Age: " . $age . "</h1>" . PHP_EOL
+    . "<h1>Resting heart rate: " . $RHR . "</h1>" . PHP_EOL;
 
 $cal = new heartRateCalculator();
 
-if (isset($_POST['submit'])){
-    $result = $cal->getRates($_POST['restHRate'], $_POST['age']);
-    foreach ($result as $row) {
-
-    }
+$result = $cal->getRates($RHR, $age);
+$output .= "<table style='width:25%'>".PHP_EOL;
+$output .= "<tr><td> <b>Intensity</b> </td> <td> <b>Ideal Rate</b> </td></tr>" . PHP_EOL;
+foreach ($result as $row){
+    $output.= "<tr>".PHP_EOL;
+    $output.= "<td>" . $row['intensity'] . "</td>".PHP_EOL."<td>" . $row['idealRate'] . "</td>" . PHP_EOL;
+    $output .= "</tr>".PHP_EOL;
 }
+
+$output.="</table>".PHP_EOL;
+
+$output.="</body>".PHP_EOL."</html>";
+
+echo $output;
